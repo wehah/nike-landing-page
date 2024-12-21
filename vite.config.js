9e2f833fb8dd086705234385412aacc3a0a82aa3
Mohamed Ali <weexaax@gmail.com>
2024-12-21 16:32:30 +1100

    update

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import svgr from 'vite-plugin-svgr';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr({ include: '**/*.svg' })],
-  base: '/nike-inspired-landing-page/',
+  base: "https://wehah.github.io/nike-landing-page",
 })
